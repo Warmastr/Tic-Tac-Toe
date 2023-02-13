a639c04b59803f6dbe5b0c75d9ac9a8ca6e98bd1
warmastr <jargyle80@yahoo.com>
2023-02-13 14:41:59 -0600

    Assigned player tokens

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,8 +3,11 @@ require './lib/player.rb'
 require './lib/token.rb'
 
 board = Board.new
+player_1_token = Token.new("X")
+player_2_token = Token.new("O")
 
+puts player_1_token
 
 board[0] = 'o'
-puts board.display_board
+puts board
 
