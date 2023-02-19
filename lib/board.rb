73a0411979443f74ca38656a90349e5b518c65b0
warmastr <jargyle80@yahoo.com>
2023-02-18 23:59:51 -0600

    Final version- refactored board array

--- lib/board.rb
+++ lib/board.rb
@@ -1,36 +1,19 @@
 class Board
   attr_accessor :board
   def initialize
-    @board = ['', '', '', '', '', '', '', '', '']
-  end
-
-  def display_board
-    row(1)
-    row(2)
-    row(3)
-    puts self
+    @board = [""] * 9
   end
 
   def to_s
     separator = "-----------"
 
     <<~BOARD
-        1   2   3
-    A    #{@board[0]} | #{@board[1]}  | #{@board[2]} 
-       #{separator}
-    B    #{@board[3]} | #{@board[4]}  | #{@board[5]} 
-       #{separator}
-    C    #{@board[6]} | #{@board[7]}  | #{@board[8]}
+          1   2   3
+      1    #{@board[0]} | #{@board[1]}  | #{@board[2]} 
+         #{separator}
+      2    #{@board[3]} | #{@board[4]}  | #{@board[5]} 
+         #{separator}
+      3    #{@board[6]} | #{@board[7]}  | #{@board[8]}
     BOARD
-  end
-
-  def []=(index, value)
-    @board[index] = value
-  end
-
-  private
-
-  def row(row_number)
-    @board.each_slice(3).to_a[row_number]
   end
 end
