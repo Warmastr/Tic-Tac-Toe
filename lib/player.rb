b7fb541ec8f12666074ea1557e1f1b682eaf1178
warmastr <jargyle80@yahoo.com>
2023-02-19 00:00:34 -0600

    Final version - added tokens

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,15 @@
 class Player
-  def initialize(player)
+  attr_writer :token
+  def initialize(player, token)
     @player = player
+    @token = token
   end
-  
+
+  def token
+    @token
+  end
+
+  def to_s
+    @player
+  end
 end
